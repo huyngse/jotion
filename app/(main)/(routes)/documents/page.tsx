faf2e4db791504c598d91613834814614c4667af
huyngse <ngogiahuy775309@gmail.com>
2025-12-06 20:22:58 +0700

    handle create a new note

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
 import { PlusCircleIcon } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 function DocumentsPage() {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const handleCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -27,8 +43,8 @@ function DocumentsPage() {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion
       </h2>
-      <Button>
-        <PlusCircleIcon className="size-4"/>
+      <Button onClick={handleCreate}>
+        <PlusCircleIcon className="size-4" />
         Create a note
       </Button>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,6 +53,7 @@ export default function RootLayout({
             storageKey="jotion-theme"
           >
             {children}
+            <Toaster position="bottom-center" />
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
